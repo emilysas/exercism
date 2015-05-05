fde2dca897b22e0dec0746683819a62d86aa3a45
Emily Sas <emily.bronwen.sas@gmail.com>
2015-05-05 15:43:31 +0100

    passing 5th test for rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -8,6 +8,17 @@ class Complement
   COMPLEMENT = {G: "C", C: "G", T: "A", A: "U"}
 
   def self.of_dna(nucleotide)
+    nucleotide.length > 1 ? complement_string(nucleotide) : return_complement(nucleotide)
+  end
+
+  def self.complement_string(nucleotide)
+    nuc_array = nucleotide.chars()
+    new_array = []
+    nuc_array.each {|nuc| new_array << return_complement(nuc)}
+    new_array.join()
+  end
+
+  def self.return_complement(nucleotide)
     COMPLEMENT[nucleotide.to_sym]
   end
 
